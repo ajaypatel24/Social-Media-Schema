b363f72135969d02b66b503ce5548106e1d721f0
ajaypatel24 <35381714+ajaypatel24@users.noreply.github.com>
2020-02-27 21:44:07 -0500

    Update Like.sql

--- Backup/Like.sql
+++ Backup/Like.sql
@@ -24,14 +24,14 @@ SET row_security = off;
 -- Data for Name: Like; Type: TABLE DATA; Schema: cs421g38; Owner: cs421g38
 --
 
-INSERT INTO cs421g38."Like" (iid) VALUES (121);
-INSERT INTO cs421g38."Like" (iid) VALUES (36);
-INSERT INTO cs421g38."Like" (iid) VALUES (31);
-INSERT INTO cs421g38."Like" (iid) VALUES (64);
-INSERT INTO cs421g38."Like" (iid) VALUES (114);
-INSERT INTO cs421g38."Like" (iid) VALUES (59);
-INSERT INTO cs421g38."Like" (iid) VALUES (126);
-INSERT INTO cs421g38."Like" (iid) VALUES (136);
+INSERT INTO cs421g38."like" (iid) VALUES (121);
+INSERT INTO cs421g38."like" (iid) VALUES (36);
+INSERT INTO cs421g38."like" (iid) VALUES (31);
+INSERT INTO cs421g38."like" (iid) VALUES (64);
+INSERT INTO cs421g38."like" (iid) VALUES (114);
+INSERT INTO cs421g38."like" (iid) VALUES (59);
+INSERT INTO cs421g38."like" (iid) VALUES (126);
+INSERT INTO cs421g38."like" (iid) VALUES (136);
 
 
 -- Completed on 2020-02-27 16:44:01
