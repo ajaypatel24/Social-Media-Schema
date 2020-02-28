5501bab7fd6b4e057b86bdd9ffb506b130d57f06
ajaypatel24 <35381714+ajaypatel24@users.noreply.github.com>
2020-02-27 21:41:22 -0500

    Update user_story.sql

--- Backup/user_story.sql
+++ Backup/user_story.sql
@@ -24,18 +24,16 @@ SET row_security = off;
 -- Data for Name: user_story; Type: TABLE DATA; Schema: cs421g38; Owner: cs421g38
 --
 
-INSERT INTO cs421g38.user_story (pid, date, "time", text, image, email, page_id, expiration_date) VALUES (14, '2020-02-14', '02:48:49', 'congue etiam justo etiam pretium', NULL, 'LorenaSheridan11@gmx.com', 1, '2020-02-14');
-INSERT INTO cs421g38.user_story (pid, date, "time", text, image, email, page_id, expiration_date) VALUES (28, '2020-02-14', '09:31:45', 'ultrices mattis odio donec vitae nisi nam', NULL, 'LorenaSheridan11@gmx.com', 2, '2020-02-14');
-INSERT INTO cs421g38.user_story (pid, date, "time", text, image, email, page_id, expiration_date) VALUES (126, '2020-02-14', '03:24:30', 'odio', NULL, 'LorenaSheridan11@gmx.com', 6, '2020-02-14');
-INSERT INTO cs421g38.user_story (pid, date, "time", text, image, email, page_id, expiration_date) VALUES (42, '2020-02-14', '08:13:45', 'at vulputate vitae nisl aenean lectus pellentesque eget nunc donec quis orci eget orci vehicula condimentum curabitur in libero', NULL, 'LorenaSheridan11@gmx.com', 2, '2020-02-14');
-INSERT INTO cs421g38.user_story (pid, date, "time", text, image, email, page_id, expiration_date) VALUES (98, '2020-02-14', '07:52:46', 'lobortis convallis tortor risus dapibus augue vel accumsan tellus nisi eu orci mauris lacinia sapien', NULL, 'LorenaSheridan11@gmx.com', 4, '2020-02-14');
-INSERT INTO cs421g38.user_story (pid, date, "time", text, image, email, page_id, expiration_date) VALUES (84, '2020-02-14', '00:25:29', 'mi', NULL, 'LorenaSheridan11@gmx.com', 4, '2020-02-14');
-INSERT INTO cs421g38.user_story (pid, date, "time", text, image, email, page_id, expiration_date) VALUES (140, '2019-08-23', '09:38:42', 'quis odio consequat varius integer ac leo pellentesque ultrices', NULL, 'MarshallShannon@gmail.com', 6, '2019-06-05');
-INSERT INTO cs421g38.user_story (pid, date, "time", text, image, email, page_id, expiration_date) VALUES (70, '2019-12-14', '01:32:27', 'pede libero quis orci nullam molestie nibh', NULL, 'MarshallShannon@gmail.com', 3, '2019-06-05');
-INSERT INTO cs421g38.user_story (pid, date, "time", text, image, email, page_id, expiration_date) VALUES (56, '2019-08-19', '16:16:06', 'neque duis bibendum morbi non quam nec dui luctus rutrum nulla tellus in sagittis dui vel', NULL, 'Mamba24@zoho.com', 3, '2019-09-24');
-INSERT INTO cs421g38.user_story (pid, date, "time", text, image, email, page_id, expiration_date) VALUES (112, '2019-09-05', '02:20:35', 'vel augue vestibulum ante ipsum primis', NULL, 'Mamba24@zoho.com', 5, '2019-09-24');
-
-
+INSERT INTO cs421g38.user_story (pid, expiration_date) VALUES (14, '2020-02-14');
+INSERT INTO cs421g38.user_story (pid, expiration_date) VALUES (28,  '2020-02-14');
+INSERT INTO cs421g38.user_story (pid, expiration_date) VALUES (126,  '2020-02-14');
+INSERT INTO cs421g38.user_story (pid, expiration_date) VALUES (42, '2020-02-14');
+INSERT INTO cs421g38.user_story (pid, expiration_date) VALUES (98, '2020-02-14');
+INSERT INTO cs421g38.user_story (pid, expiration_date) VALUES (84, '2020-02-14');
+INSERT INTO cs421g38.user_story (pid, expiration_date) VALUES (140, '2019-06-05');
+INSERT INTO cs421g38.user_story (pid, expiration_date) VALUES (70,  '2019-06-05');
+INSERT INTO cs421g38.user_story (pid, expiration_date) VALUES (56,  '2019-09-24');
+INSERT INTO cs421g38.user_story (pid, expiration_date) VALUES (112, '2019-09-24');
 -- Completed on 2020-02-27 16:59:32
 
 --
