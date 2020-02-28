361486abee9f5bb40adc6da488f4ee288f6adbba
ajaypatel24 <35381714+ajaypatel24@users.noreply.github.com>
2020-02-27 21:39:16 -0500

    Update advertisement.sql

--- Backup/advertisement.sql
+++ Backup/advertisement.sql
@@ -24,12 +24,12 @@ SET row_security = off;
 -- Data for Name: advertisement; Type: TABLE DATA; Schema: cs421g38; Owner: cs421g38
 --
 
-INSERT INTO cs421g38.advertisement (pid, date, "time", text, image, email, page_id, price) VALUES (110, '2019-06-07', '08:20:51', 'duis aliquam convallis nunc proin at turpis a pede posuere nonummy', NULL, 'CobyBax8@gmail.com', 5, 15);
-INSERT INTO cs421g38.advertisement (pid, date, "time", text, image, email, page_id, price) VALUES (22, '2020-01-01', '19:25:51', 'interdum in ante vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae duis', NULL, 'GordonHaywardBrother@gmail.com', 1, 15);
-INSERT INTO cs421g38.advertisement (pid, date, "time", text, image, email, page_id, price) VALUES (44, '2019-10-15', '15:13:52', 'pede morbi porttitor', NULL, 'Anish1994@gmail.com', 2, 15);
-INSERT INTO cs421g38.advertisement (pid, date, "time", text, image, email, page_id, price) VALUES (88, '2019-06-09', '01:04:33', 'cras mi pede malesuada in', NULL, 'Mamba24@zoho.com', 4, 15);
-INSERT INTO cs421g38.advertisement (pid, date, "time", text, image, email, page_id, price) VALUES (66, '2019-03-14', '07:41:30', 'integer ac leo', NULL, 'Marleeelove@live.com', 3, 15);
-INSERT INTO cs421g38.advertisement (pid, date, "time", text, image, email, page_id, price) VALUES (132, '2019-04-09', '05:05:17', 'eu interdum eu tincidunt in leo maecenas pulvinar lobortis est phasellus sit amet erat nulla tempus vivamus', NULL, 'Marleeelove@live.com', 6, 15);
+INSERT INTO cs421g38.advertisement (pid, price) VALUES (110, 15);
+INSERT INTO cs421g38.advertisement (pid, price) VALUES (22,  15);
+INSERT INTO cs421g38.advertisement (pid, price) VALUES (44,  15);
+INSERT INTO cs421g38.advertisement (pid, price) VALUES (88, 15);
+INSERT INTO cs421g38.advertisement (pid, price) VALUES (66, 15);
+INSERT INTO cs421g38.advertisement (pid, price) VALUES (132, 15);
 
 
 -- Completed on 2020-02-27 16:50:30
