6a1957c7fa6ffffeb052e244809d33431d659b4e
ajaypatel24 <35381714+ajaypatel24@users.noreply.github.com>
2020-02-27 20:11:58 -0500

    Update SocialMediaDatabase.sql

--- SocialMediaDatabase.sql
+++ SocialMediaDatabase.sql
@@ -1,12 +1,8 @@
-
-
 CREATE TABLE admin (
 	employee_id integer NOT NULL UNIQUE,
 	access_rights varchar(15),
 	PRIMARY KEY(employee_id)
 );
-
-
 
 CREATE TABLE accountuser (
 	email varchar(320) NOT NULL UNIQUE,
@@ -15,7 +11,6 @@ CREATE TABLE accountuser (
 	password varchar(320),
 	PRIMARY KEY(email)
 );
-
 
 CREATE TABLE page (
 	page_id integer,
@@ -35,9 +30,28 @@ CREATE TABLE event (
     FOREIGN KEY (organizer) REFERENCES accountuser(email)
 );
 
+CREATE TABLE post (
+	pid integer NOT NULL,
+	date date NOT NULL,
+	time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+	text varchar(200),
+	image varchar(20),
+	email varchar(320),
+	page_id integer,
+    PRIMARY KEY(pid),
+    FOREIGN KEY (email) REFERENCES accountuser(email),
+    FOREIGN KEY (page_id) REFERENCES page(page_id)
+);
+
 CREATE TABLE interaction (
 	iid integer NOT NULL UNIQUE, 
-	PRIMARY KEY(iid)
+	page_id integer,
+	pid integer,
+	email varchar(320),
+	PRIMARY KEY(iid),
+	FOREIGN KEY (email) REFERENCES accountuser(email),
+	FOREIGN KEY (page_id) REFERENCES page(page_id),
+	FOREIGN KEY (pid) REFERENCES post(pid)
 );
 
 CREATE TABLE "like" (
@@ -59,23 +73,7 @@ CREATE TABLE share (
     FOREIGN KEY (iid) REFERENCES Interaction(iid)
 );
 
-
-
-CREATE TABLE post (
-	pid integer NOT NULL,
-	date date NOT NULL,
-	time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-	text varchar(200),
-	image varchar(20),
-	email varchar(320),
-	page_id integer,
-    PRIMARY KEY(pid),
-    FOREIGN KEY (email) REFERENCES accountuser(email),
-    FOREIGN KEY (page_id) REFERENCES page(page_id)
-);
-
-
-CREATE TABLE interacts ( --change this
+CREATE TABLE interacts (
 	pid integer NOT NULL UNIQUE,
 	email varchar(320) NOT NULL UNIQUE,
 	PRIMARY KEY (pid,email),
@@ -84,7 +82,6 @@ CREATE TABLE interacts ( --change this
     
 );
 
-
 CREATE TABLE is_friend_with ( 
 	email varchar(320) NOT NULL UNIQUE,
 	friend_email varchar(320) NOT NULL,
@@ -92,7 +89,12 @@ CREATE TABLE is_friend_with (
 	FOREIGN KEY (friend_email) REFERENCES accountuser(email)
 );
 
-CREATE TABLE makes ( --change this
+CREATE TABLE makes (
+	page_id integer,
+	pid integer,
+	PRIMARY KEY (page_id, pid),
+	FOREIGN KEY (page_id) REFERENCES Page(page_id),
+	FOREIGN KEY (pid) REFERENCES post(pid)
 	
 );
 
@@ -117,9 +119,9 @@ CREATE TABLE notification (
 	time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
 	type varchar(20),
 	iid integer,
-	PRIMARY KEY (date,time)
+	PRIMARY KEY (date,time),
+	FOREIGN KEY (iid) REFERENCES Interaction(iid)
 );
-
 
 CREATE TABLE partakes (
 	page_id integer NOT NULL ,
@@ -129,7 +131,6 @@ CREATE TABLE partakes (
     FOREIGN KEY (email) REFERENCES accountuser(email)
 );
 
-
 CREATE TABLE participates (
 	eid integer NOT NULL ,
 	email varchar(320) NOT NULL ,
@@ -137,9 +138,6 @@ CREATE TABLE participates (
     FOREIGN KEY (eid) REFERENCES event(eid),
     FOREIGN KEY (email) REFERENCES accountuser(email)
 );
-
-
-
 
 CREATE TABLE advertisement (
 	pid integer NOT NULL,
@@ -153,8 +151,7 @@ CREATE TABLE advertisement (
     PRIMARY KEY(pid),
     FOREIGN KEY (email) REFERENCES accountuser(email),
     FOREIGN KEY (page_id) REFERENCES page(page_id),
-    FOREIGN KEY (pid) REFERENCES post(pid)
-    
+    FOREIGN KEY (pid) REFERENCES post(pid)    
 );
 
 CREATE TABLE user_story (
