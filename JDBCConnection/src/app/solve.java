aa37cf8dfcef03baa1da7469f9bc91a3fb571e03
Ajay <ajay.patel@mail.mcgill.ca>
2020-04-07 14:48:04 -0400

    GUI changes

--- JDBCConnection/src/app/solve.java
+++ JDBCConnection/src/app/solve.java
@@ -20,8 +20,7 @@ class solve extends JFrame implements ItemListener, ActionListener {
 	private static final long serialVersionUID = 1L;
 	
 	
-	public solve() {
-	} 
+
 
 	// frame 
 	static JFrame f; 
@@ -30,13 +29,20 @@ class solve extends JFrame implements ItemListener, ActionListener {
 	static JLabel l, l1; 
 
 	// combobox 
-	static JComboBox c1; 
+	static JComboBox comboBox; 
 
+	static JTextField FirstName;
+	static JTextField LastName;
+	static JTextField Email;
+	static JTextField Password;
+	
 	static JButton b1;
+	static JButton b2;
 	
 	static JTable j;
+	
 	// main class 
-	public static void main(String[] args) throws SQLException 
+	public static void main (String [] args) throws SQLException 
 	{ 
 		// create a new frame 
 		
@@ -50,36 +56,117 @@ class solve extends JFrame implements ItemListener, ActionListener {
 
 		@SuppressWarnings("resource")
 		Scanner stdin = new Scanner (System.in);
-		System.out.print("Username: ");
-		String user = stdin.next();
+		//System.out.print("Username: ");
+		//String user = stdin.next();
 		
 		System.out.print("Password: ");
 		String pass = stdin.next();
 		
 		String url = "jdbc:postgresql://comp421.cs.mcgill.ca:5432/cs421";
-		Connection con = DriverManager.getConnection(url, user, pass);
+		Connection con = DriverManager.getConnection(url, "cs421g38", pass);
 		Statement statement = con.createStatement();
 		System.out.println("Successful Connection");
-		//Connection End
-
-		// Data to be displayed in the JTable 
-        String[][] data = { 
-            { "Test", "4031", "CSE" }, 
-            { "Test2", "6014", "IT" } 
-        }; 
-  
-        // Column Names 
-        String[] columnNames = { "Name", "Roll Number", "Department" }; 
+		//Connection Ends
   
         // Initializing the JTable 
-        j = new JTable(data, columnNames); 
-        j.setBounds(30, 40, 200, 300);
-
+        f = new JFrame("frame"); 
+        f.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+        f.getContentPane().setLayout(null);
+    	
+    	JLayeredPane layeredPane = new JLayeredPane();
+    	layeredPane.setBounds(10, 47, 403, 205);
+    	f.getContentPane().add(layeredPane);
+    	layeredPane.setLayout(new CardLayout(0, 0));
+    	
+    	
+    	
+    	
+    	JLabel lblNewLabel = new JLabel("Add new user");
+    	JLabel lblNewLabel2 = new JLabel("Opt 2 selected");
+    	JLabel lblNewLabel3 = new JLabel("Opt 3 selected");
+    	JLabel lblNewLabel4 = new JLabel("Opt 4 selected");
+    	
+    	//Add User
+    	JLabel FirstName = new JLabel("First Name");
+    	JLabel LastName = new JLabel("Last Name");
+    	JLabel Email = new JLabel("Email");
+    	JLabel Password = new JLabel("Password");
+    	
+    	JTextField FirstNameField = new JTextField(20);
+    	JTextField LastNameField = new JTextField(20);
+    	JTextField EmailField = new JTextField(20);
+    	JTextField PasswordField = new JTextField(20);
+    	
+    	
+    	JLabel Tester = new JLabel("Tester:");
+    	
+    	JPanel Option1 = new JPanel();
+    	BoxLayout layout1 = new BoxLayout(Option1, BoxLayout.Y_AXIS);
+    	Option1.setLayout(layout1);
+    	Option1.add(lblNewLabel);
+    	Option1.add(FirstName);
+    	Option1.add(FirstNameField);
+    	Option1.add(LastName);
+    	Option1.add(LastNameField);
+    	Option1.add(Email);
+    	Option1.add(EmailField);
+    	Option1.add(Password);
+    	Option1.add(PasswordField);
+    	layeredPane.add(Option1, "name_2074080574799800");
+    	
+    	JPanel Option2 = new JPanel();
+    	
+    	BoxLayout layout2 = new BoxLayout(Option2, BoxLayout.Y_AXIS);
+    	Option2.setLayout(layout2);
+    	Option2.add(Tester);
+    	layeredPane.add(Option2, "name_2074097485636900");
+    	
+    	JPanel Option3 = new JPanel();
+    	/*
+    	BoxLayout layout3 = new BoxLayout(Option3, BoxLayout.Y_AXIS);
+    	Option3.setLayout(layout3);
+    	Option3.add(lblNewLabel);
+    	Option3.add(FirstName);
+    	Option3.add(FirstNameField);
+    	Option3.add(LastName);
+    	Option3.add(LastNameField);
+    	Option3.add(Email);
+    	Option3.add(EmailField);
+    	Option3.add(Password);
+    	Option3.add(PasswordField);
+    	layeredPane.add(Option3, "name_2074111329535300");
+    	
+    	JPanel Option4 = new JPanel();
+    	BoxLayout layout4 = new BoxLayout(Option4, BoxLayout.Y_AXIS);
+    	Option4.setLayout(layout4);
+    	Option4.add(lblNewLabel);
+    	Option4.add(FirstName);
+    	Option4.add(FirstNameField);
+    	Option4.add(LastName);
+    	Option4.add(LastNameField);
+    	Option4.add(Email);
+    	Option4.add(EmailField);
+    	Option4.add(Password);
+    	Option4.add(PasswordField);
+    	layeredPane.add(Option4, "name_2074113974014000");
+    	
+    	*/
+    	
+    	
+    	Option2.add(lblNewLabel2);
+    	//Option3.add(lblNewLabel3);
+    	//Option4.add(lblNewLabel4);
+    	
+    	
+    	String s1[] = { "Option 1", "Option 2", "Option 3", "Option 4", "Quit"}; 
+    	@SuppressWarnings("unchecked")
+		JComboBox comboBox = new JComboBox(s1);
+    	comboBox.setBounds(311, 11, 102, 22);
+    	f.getContentPane().add(comboBox);
+ 
 		
 		
-		
-		f = new JFrame("frame"); 
-
+		JPanel p = new JPanel(); 
 		// create a object 
 		solve s = new solve(); 
 
@@ -87,25 +174,51 @@ class solve extends JFrame implements ItemListener, ActionListener {
 		f.getContentPane().setLayout(new FlowLayout()); 
 
 		// array of string contating cities 
-		String s1[] = { "Option 1", "Option 2", "Option 3", "Option 4", "Quit"}; 
+		
 
 		// create checkbox 
-		c1 = new JComboBox(s1); 
 		b1 = new JButton("Confirm");
 		// add ItemListener 
 		b1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				int sqlCode = 0; // Variable to hold SQLCODE
 				String sqlState = "00000"; // Variable to hold SQLSTATE
-				String opt = c1.getSelectedItem()+"";
-				switch(opt) {
-				case "Option 1":
+				String opt = comboBox.getSelectedItem()+"";
+				if(opt.equals("Option 1")) {
+					layeredPane.removeAll();
+					layeredPane.add(Option1);
+					layeredPane.repaint();
+					layeredPane.revalidate();
+					System.out.println("hello");
+					String First = FirstNameField.getText();
+					String Last = LastNameField.getText();
+					String Email = EmailField.getText();
+					String Pass = PasswordField.getText();
+					System.out.println(First + " " + Last + " " + Email + " " + Pass);
+					String insertSQL = "INSERT INTO User VALUES ( " + First + "," + Last + "," + Email + "," + Pass +")";
 					try {
-						String querySQL = "SELECT * FROM person;";
+						statement.executeUpdate(insertSQL);
+					} catch (SQLException e1) {
+						sqlCode = e1.getErrorCode();
+						sqlState = e1.getSQLState();
+						System.out.println("SQLcode: " + sqlCode);
+						System.out.println("SQLState: " + sqlState);
+					}
+					System.out.println("Successful Insert");
+					
+					
+					}	
+				if(opt.equals("Option 2")) {
+					layeredPane.removeAll();
+					layeredPane.add(Option2);
+					layeredPane.repaint();
+					layeredPane.revalidate();
+					try {
+						String querySQL = "SELECT * FROM admin;";
 						System.out.println(querySQL);
 						java.sql.ResultSet rs = statement.executeQuery(querySQL);
 						while (rs.next()) {
-							String name = rs.getString("pid");
+							String name = rs.getString("employee_id");
 							System.out.println(name);
 							l1.setText(name); 
 						}
@@ -115,14 +228,23 @@ class solve extends JFrame implements ItemListener, ActionListener {
 						sqlState = err.getSQLState(); 
 						System.out.println("Code: " + sqlCode + "  sqlState: " + sqlState);
 					}
-					break;
-				case "Option 2":
-					break;
-				case "Option 3":
-					break;
-				case "Option 4":
-					break;
-				case "Quit": 
+				}
+				if(opt.equals("Option 3")) {
+					layeredPane.removeAll();
+					//layeredPane.add(Option3);
+					layeredPane.repaint();
+					layeredPane.revalidate();
+					l1.setText("3"); 
+				}
+				if(opt.equals("Option 4")) {
+					layeredPane.removeAll();
+					//layeredPane.add(Option4);
+					layeredPane.repaint();
+					layeredPane.revalidate();
+					l1.setText("4"); 
+				}
+				if(opt.contentEquals("Quit")) {
+				
 					try {
 						statement.close();
 						con.close();
@@ -132,14 +254,14 @@ class solve extends JFrame implements ItemListener, ActionListener {
 					}
 					
 					System.exit(0);
-					break;
+					
 				}
 				
 				
-				System.out.println(""+c1.getSelectedItem()+"");
+				System.out.println(""+comboBox.getSelectedItem()+"");
 			}
 		});
-		c1.addItemListener(s); 
+		comboBox.addItemListener(s); 
 
 		// create labels 
 		l = new JLabel("Select an option "); 
@@ -150,16 +272,14 @@ class solve extends JFrame implements ItemListener, ActionListener {
 		l1.setForeground(Color.blue); 
 
 		// create a new panel 
-		JPanel p = new JPanel(); 
+		
 
 		p.add(l); 
 
 		// add combobox to panel 
-		p.add(c1); 
 		
 		p.add(l1); 
 		p.add(b1);
-		p.add(j);
 		// add panel to frame 
 		f.getContentPane().add(p); 
 
@@ -168,18 +288,21 @@ class solve extends JFrame implements ItemListener, ActionListener {
 
 		f.show(); 
 	} 
+	
+
 	public void itemStateChanged(ItemEvent e) 
 	{ 
 		// if the state combobox is changed 
-		if (e.getSource() == c1) { 
+		if (e.getSource() == comboBox) { 
 			
-			l1.setText(""+c1.getSelectedItem()+""); 
+			l1.setText(""+comboBox.getSelectedItem()+""); 
 		} 
 		
 			
 		
 	
 	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
